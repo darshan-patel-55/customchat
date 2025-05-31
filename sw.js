865bba1ee91ea3c5adfb65adb81cba03e712a1b9
Darshan Patel <darshan.patel@bista-it-00310.bistasolutions.com>
2025-05-31 14:14:16 +0530

    Add PWA

--- sw.js
+++ sw.js
@@ -1,47 +1,24 @@
+// Basic service worker that caches static assets
 const CACHE_NAME = 'dnd-chat-v1';
-const urlsToCache = [
+const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
-  '/favicon.ico',
-  // Add other assets you want to cache
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
-
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .then((response) => response || fetch(event.request))
   );
 });
 
@@ -54,34 +31,34 @@ self.addEventListener('activate', event => {
 
 
 
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
 
-  event.respondWith(
-    caches.match(event.request)
-      .then(cached => {
-        const networked = fetch(event.request)
-          .then(fetchedFromNetwork, unableToResolve)
-          .catch(unableToResolve);
 
-        return cached || networked;
 
-        function fetchedFromNetwork(response) {
-          const cacheCopy = response.clone();
-          caches.open(CACHE_NAME)
-            .then(cache => cache.put(event.request, cacheCopy));
-          return response;
-        }
 
-        function unableToResolve() {
-          return new Response('<h1>Service Unavailable</h1><p>You are offline. Some features may not be available.</p>', {
-            status: 503,
-            statusText: 'Service Unavailable',
-            headers: new Headers({
-              'Content-Type': 'text/html'
-            })
-          });
-        }
-      })
-  );
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
