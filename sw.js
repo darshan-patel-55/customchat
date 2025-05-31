9f9b64b4f3b231fcafa754d473d635cb007d9d21
Darshan Patel <darshan.patel@bista-it-00310.bistasolutions.com>
2025-05-31 14:26:46 +0530

    Add PWA

--- sw.js
+++ sw.js
@@ -1,64 +1,21 @@
-// Basic service worker that caches static assets
 const CACHE_NAME = 'dnd-chat-v1';
 const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png'
+  '/customchat/',
+  '/customchat/index.html',
+  '/customchat/manifest.json',
+  '/customchat/icons/icon-192x192.png',
+  '/customchat/icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
-  );
-});
+      .then(cache => cache.addAll(ASSETS_TO_CACHE))
+);
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+      .then(response => response || fetch(event.request))
   );
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
