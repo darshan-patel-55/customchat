0b9994ce954bb21b118f6ccc557d82874ccf6821
Darshan Patel <darshan.patel@bista-it-00310.bistasolutions.com>
2025-06-01 10:51:23 +0530

    Improve Calculator Design install app button

--- sw.js
+++ sw.js
@@ -1,31 +1,3 @@
-//const CACHE_NAME = 'dnd-chat-v1';
-//const ASSETS_TO_CACHE = [
-//  '/customchat/index.html',
-//  '/customchat/manifest.json',
-//  '/customchat/favicon.ico'
-//];
-//
-//// Install Service Worker & Cache Files
-//self.addEventListener("install", (event) => {
-//  event.waitUntil(
-//    caches.open(CACHE_NAME)
-//      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
-//  );
-//});
-//
-//// Fetch Cached Files (Offline Support)
-//self.addEventListener("fetch", (event) => {
-//  event.respondWith(
-//    caches.match(event.request)
-//      .then((response) => response || fetch(event.request))
-//  );
-//});
-
-
-
-
-
-
 const CACHE_NAME = 'dnd-chat-v2';
 const ASSETS_TO_CACHE = [
   '/customchat/',
